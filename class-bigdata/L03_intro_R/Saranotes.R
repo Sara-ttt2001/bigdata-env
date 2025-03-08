ca0ac2880201c62d734fbedf73d636140177139e
Sara-ttt2001 <saraemadabdelk.altamam01@universitadipavia.it>
2025-03-08 23:56:08 +0000

    studying

--- class-bigdata/L03_intro_R/Saranotes.R
+++ class-bigdata/L03_intro_R/Saranotes.R
@@ -5,14 +5,15 @@
 # for characters we use double quotations
 # a vector only contains one type (you can convert a number to a character and not the opposite)
 # we can extract an element from a string through slicing []
-# minute 57
+# single [], we return an object of the same type that you are slicing
+# double[], we return an object of the type of the element that you are taking
 # lists: collection of other objects, could be vector, tables, a mix of them with lists
-#structure of the object is str
-#matrix is another shape of object: 2D object (a collection of vectors)
+# structure of the object is str
+# matrix is another shape of object: 2D object (a collection of vectors)
 # data frame: a data science object using data.frame, the data should be in the columns we have the variables (parameters) we are measuring
 # and in the rows, we have the records or measurements (min 1:23:00) and the cell is the measurement of that variable
 # the rows must be uniquely identifiable
 # load package using library
-#another data structure is tibbles
+# another data structure is tibbles
 # data transformation is crucial for modelling
 # %>% command + shift + M (acts as a pipe, combining multiple operations together)
---
+++ class-bigdata/L03_intro_R/practice.R
@@ -0,0 +1,38 @@
+a = 1
+b <- 2
+c = a + b
+c
+
+sara = c(30,30,30)
+ammar = c(2,3,7)
+
+total = sara[1] + ammar[3]
+total
+sara[1]
+print(total)
+sara[c(1,2,3)]
+
+hushhush = list(
+  first = c(23,56,79,147),
+  second = c("SARA","AMMAR","AUM"),
+  third = c(984,1024,57)
+)
+
+store_first = hushhush[[1]] #store_first is now a vector that is not within the list 
+store_first
+print(store_first[3])
+
+store_third = hushhush[[3]]
+store_third
+
+sum = store_first[3] + store_third[2]
+sum
+
+
+pepsi = matrix(c(678,456,903,345), ncol = 2, byrow = 2)
+cola = matrix(c(76,54,90,99), ncol = 2, byrow = 2)
+pepsi
+cola
+
+product = pepsi[1,2] * cola[2,1]
+product
