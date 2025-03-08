5440ca70a9005228f263142b48e5d887c8422cf8
Sara-ttt2001 <saraemadabdelk.altamam01@universitadipavia.it>
2025-03-08 01:14:14 +0000

    saving code

--- class-bigdata/L03_intro_R/intro_script.R
+++ class-bigdata/L03_intro_R/intro_script.R
@@ -65,8 +65,8 @@ is.list(list_a[[1]])
 
 str(list_a[[1]])
 
-str(list_a)
-str(list_a[1])
+str(list_a) #list of 3 elements
+str(list_a[1]) #list of 1 element
 
 #################################
 ## MATRICES ####################
